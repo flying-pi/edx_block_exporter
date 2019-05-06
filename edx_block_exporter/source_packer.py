20665a41847c17db6d3f3249aeae8c4590b64ce6
yurabraiko <yurabraiko@gmail.com>
2019-05-07 02:08:39 +0300

    added path getter

--- edx_block_exporter/source_packer.py
+++ edx_block_exporter/source_packer.py
@@ -49,51 +49,53 @@ class XBlockDecomposer(object):
             fields.append(XBlockField(**field_info))
         return fields
 
-    @staticmethod
-    def _root_module_name(module_path):
-        return module_path.split('.')[0]
+    def _get_dep_modules_name(self, module):
 
-    @staticmethod
-    def _is_system_name(attr_name):
-        """
+        package_to_discover = [ import_module(module.__module__.split('.')[0])]
+        module_names = [module.__module__]
 
-        :param str attr_name :
-        :return:
-        """
-        return not (attr_name.startswith("__") and attr_name.startswith("__"))
-
-    @staticmethod
-    def _get_recursive_root_path_dep(module_obj):
-        result = set()
-        try:
-            for member_name in dir(module_obj):
-                member = getattr(module_obj, member_name)
-                if inspect.ismodule(member):
-                    result.add(XBlockDecomposer._root_module_name(member.__name__))
-                    result.update(XBlockDecomposer._get_recursive_root_path_dep(member))
-        #             TODO do something with circle imports
-        except:
-
-            import pydevd_pycharm
-            pydevd_pycharm.settrace('host.docker.internal', port=3758, stdoutToServer=True, stderrToServer=True)
-            print('asasasasas')
-        return result
-
-    def _get_relative_pathes(self, module):
-        package_to_discover = {self._root_module_name(module.__module__)}
-        module_path_mapping = {}
         while package_to_discover:
-            module_name = package_to_discover.pop()
-            while module_name in module_path_mapping:
-                module_name = package_to_discover.pop()
-            imported_module = import_module(module_name)
-            module_path_mapping[module_name] = '/'.join(os.path.abspath(imported_module.__file__).split('/')[:-1])+'/'
-            package_to_discover.update(self._get_recursive_root_path_dep(imported_module))
+            iteration_module = package_to_discover.pop()
+            for member_name in dir(iteration_module):
+                try:
+                    member = getattr(iteration_module, member_name)
+                except:
+                    # NOTE(flyingpi): Need to take decision, dies we need certain exception type
+                    # FIXME(flyingpi): logs required
+                    continue
+                if not inspect.ismodule(member):
+                    continue
+                member_module_name = member.__name__
+                if member_module_name in module_names:
+                    continue
+                module_names.append(member_module_name)
+                package_to_discover.append(member)
+                root_module = member_module_name.split('.')[0]
+                if root_module == member_module_name:
+                    continue
+                module_names.append(root_module)
+                package_to_discover.append(import_module(root_module))
+
+        unique_root_modules_name = {name.split('.')[0] for name in module_names}
+        return list(unique_root_modules_name)
+
+    def _get_relative_pathes(self, dep_modules):
+        c = 0
+        for i in dep_modules:
+            try:
+                import_module(i).__file__
+            except:
+                print c
+                print i
+            c+=1
+        imp.find_module('heapq')
+        import pydevd_pycharm
+        pydevd_pycharm.settrace('host.docker.internal', port=3758, stdoutToServer=True, stderrToServer=True)
+        print('asasasasas')
 
 
-            import pydevd_pycharm
-            pydevd_pycharm.settrace('host.docker.internal', port=3758, stdoutToServer=True, stderrToServer=True)
-            print('asasasasas')
+
+
 
 
 
@@ -101,9 +103,8 @@ class XBlockDecomposer(object):
     def get_xblock_info(self):
         module = self.block.unmixed_class
         fields = self._get_fields_info(module)
-        dep_file_pathes = self._get_relative_pathes(module)
-
-
+        dep_modules = self._get_dep_modules_name(module)
+        dep_file_pathes = self._get_relative_pathes(dep_modules)
 
 """
 import sys, traceback
